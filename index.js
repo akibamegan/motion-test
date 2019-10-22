422b8d7ad816dc7f59902ce203456af1fb8702ce
Megan Akiba <33731208+akibamegan@users.noreply.github.com>
2019-10-22 13:50:58 -0600

    starting websockets

---
+++ index.js
@@ -0,0 +1,59 @@
+var socket = io();
+
+
+var app = require('express')();
+var httpSocketServer = require('http').createServer(app);
+var io = require('socket.io')(httpSocketServer);
+
+app.get('/', function(req, res){
+	res.sendFile('index.html', { root: __dirname });
+});
+
+io.on('connection', function(socket){
+	console.log('a user has connected');
+	socket.emit('alert', { message: 'someone has connected'});
+});
+
+
+// listens for socket connection
+httpSocketServer.listen(3000, function(){
+	console.log('listening on *:3000');
+});
+
+
+
+
+
+function captureAcceleration() {
+	if (!('ondevicemotion' in window)) {
+		document.getElementById('dm-unsupported').classList.remove('hidden');
+	} 
+	else {
+		document.getElementById('dm-info').classList.remove('hidden');
+
+		window.addEventListener('devicemotion', function(event) {
+			document.getElementById('acceleration-x').innerHTML = Math.round(event.acceleration.x);
+			document.getElementById('acceleration-y').innerHTML = Math.round(event.acceleration.y);
+			document.getElementById('acceleration-z').innerHTML = Math.round(event.acceleration.z);
+		});
+	}
+}
+
+function askPermission(){
+	if(typeof DeviceMotionEvent.requestPermission === 'function') {
+		DeviceMotionEvent.requestPermission()
+		.then(permissionState => {
+			if(permissionState === 'granted') {
+				window.addEventListener('devicemotion', () => {
+						// do something
+						captureAcceleration();
+					});
+			}
+		})
+		.catch(console.error);
+	}
+	else {
+			// regular non ios 13 devices
+			captureAcceleration();
+		}
+	}
